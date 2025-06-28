4c6bd005f2d33106190592f750a68ae6f1a29e5d
Your Name <youremail@example.com>
2025-06-28 04:01:44 +0300

    fix: correct import for create_app instead of app

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from hemtna1.app import app, socketio
+from hemtna1.app import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
