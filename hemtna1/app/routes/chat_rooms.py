5d27d41edee54e1769c1a4785d96a6928ba1b5e3
Your Name <youremail@example.com>
2025-06-29 06:10:59 +0300

    fix: update config.py for JWT or DB settings

--- hemtna1/app/routes/chat_rooms.py
+++ hemtna1/app/routes/chat_rooms.py
@@ -1,95 +1,95 @@
-from flask import Blueprint, request, url_for
-from hemtna1.app import db, socketio  # ✅ استيراد من الحزمة الصحيحة
-from hemtna1.app.models import Room, RoomUser, User
-from flask_socketio import emit, join_room, leave_room
+# from flask import Blueprint, request, url_for
+# from hemtna1.app import db, socketio  # ✅ استيراد من الحزمة الصحيحة
+# from hemtna1.app.models import Room, RoomUser, User
+# from flask_socketio import emit, join_room, leave_room
 
-chat_rooms_bp = Blueprint('chat_rooms', __name__)
+# chat_rooms_bp = Blueprint('chat_rooms', __name__)
 
-@socketio.on('create_room')
-def handle_create_room(data):
-    name = data['name']
-    doctor_id = data['doctor_id']
-    image = data.get('image')
-    room = Room(name=name, doctor_id=doctor_id, image=image)
-    db.session.add(room)
-    db.session.commit()
-    emit('room_created', {
-        'id': room.id,
-        'name': room.name or "",
-        'image': url_for('static', filename='profile_pics/' + (room.image if room.image else 'default.png'), _external=True),
-        'doctor_id': room.doctor_id or 0
-    }, broadcast=True)
+# @socketio.on('create_room')
+# def handle_create_room(data):
+#     name = data['name']
+#     doctor_id = data['doctor_id']
+#     image = data.get('image')
+#     room = Room(name=name, doctor_id=doctor_id, image=image)
+#     db.session.add(room)
+#     db.session.commit()
+#     emit('room_created', {
+#         'id': room.id,
+#         'name': room.name or "",
+#         'image': url_for('static', filename='profile_pics/' + (room.image if room.image else 'default.png'), _external=True),
+#         'doctor_id': room.doctor_id or 0
+#     }, broadcast=True)
 
-@socketio.on('add_user_to_room')
-def handle_add_user_to_room(data):
-    room_id = data['room_id']
-    user_id = data['user_id']
-    if not RoomUser.query.filter_by(room_id=room_id, user_id=user_id).first():
-        ru = RoomUser(room_id=room_id, user_id=user_id)
-        db.session.add(ru)
-        db.session.commit()
-    emit('user_added_to_room', {'room_id': room_id, 'user_id': user_id}, room=f'room_{room_id}')
+# @socketio.on('add_user_to_room')
+# def handle_add_user_to_room(data):
+#     room_id = data['room_id']
+#     user_id = data['user_id']
+#     if not RoomUser.query.filter_by(room_id=room_id, user_id=user_id).first():
+#         ru = RoomUser(room_id=room_id, user_id=user_id)
+#         db.session.add(ru)
+#         db.session.commit()
+#     emit('user_added_to_room', {'room_id': room_id, 'user_id': user_id}, room=f'room_{room_id}')
 
-@socketio.on('remove_user_from_room')
-def handle_remove_user_from_room(data):
-    ru = RoomUser.query.filter_by(room_id=data['room_id'], user_id=data['user_id']).first()
-    if ru:
-        db.session.delete(ru)
-        db.session.commit()
-    emit('user_removed_from_room', {'room_id': data['room_id'], 'user_id': data['user_id']}, room=f'room_{data["room_id"]}')
+# @socketio.on('remove_user_from_room')
+# def handle_remove_user_from_room(data):
+#     ru = RoomUser.query.filter_by(room_id=data['room_id'], user_id=data['user_id']).first()
+#     if ru:
+#         db.session.delete(ru)
+#         db.session.commit()
+#     emit('user_removed_from_room', {'room_id': data['room_id'], 'user_id': data['user_id']}, room=f'room_{data["room_id"]}')
 
-@socketio.on('delete_room')
-def handle_delete_room(data):
-    room = Room.query.get(data['room_id'])
-    if room:
-        db.session.delete(room)
-        db.session.commit()
-    emit('room_deleted', {'room_id': data['room_id']}, broadcast=True)
+# @socketio.on('delete_room')
+# def handle_delete_room(data):
+#     room = Room.query.get(data['room_id'])
+#     if room:
+#         db.session.delete(room)
+#         db.session.commit()
+#     emit('room_deleted', {'room_id': data['room_id']}, broadcast=True)
 
-@socketio.on('edit_room')
-def handle_edit_room(data):
-    room = Room.query.get(data['room_id'])
-    if room:
-        if 'name' in data:
-            room.name = data['name']
-        if 'image' in data:
-            room.image = data['image']
-        db.session.commit()
-        emit('room_edited', {
-            'room_id': room.id,
-            'name': room.name or "",
-            'image': url_for('static', filename='profile_pics/' + (room.image if room.image else 'default.png'), _external=True)
-        }, broadcast=True)
+# @socketio.on('edit_room')
+# def handle_edit_room(data):
+#     room = Room.query.get(data['room_id'])
+#     if room:
+#         if 'name' in data:
+#             room.name = data['name']
+#         if 'image' in data:
+#             room.image = data['image']
+#         db.session.commit()
+#         emit('room_edited', {
+#             'room_id': room.id,
+#             'name': room.name or "",
+#             'image': url_for('static', filename='profile_pics/' + (room.image if room.image else 'default.png'), _external=True)
+#         }, broadcast=True)
 
-@socketio.on('get_rooms')
-def handle_get_rooms(data):
-    doctor_id = data.get('doctor_id')
-    user_id = data.get('user_id')
-    if doctor_id:
-        rooms = Room.query.filter_by(doctor_id=doctor_id).all()
-    elif user_id:
-        room_ids = [ru.room_id for ru in RoomUser.query.filter_by(user_id=user_id).all()]
-        rooms = Room.query.filter(Room.id.in_(room_ids)).all()
-    else:
-        rooms = Room.query.all()
-    result = []
-    for room in rooms:
-        users = [ru.user_id for ru in room.users]
-        result.append({
-            'id': room.id,
-            'name': room.name or "",
-            'image': url_for('static', filename='profile_pics/' + (room.image if room.image else 'default.png'), _external=True),
-            'doctor_id': room.doctor_id or 0,
-            'users': users
-        })
-    emit('rooms_list', result)
+# @socketio.on('get_rooms')
+# def handle_get_rooms(data):
+#     doctor_id = data.get('doctor_id')
+#     user_id = data.get('user_id')
+#     if doctor_id:
+#         rooms = Room.query.filter_by(doctor_id=doctor_id).all()
+#     elif user_id:
+#         room_ids = [ru.room_id for ru in RoomUser.query.filter_by(user_id=user_id).all()]
+#         rooms = Room.query.filter(Room.id.in_(room_ids)).all()
+#     else:
+#         rooms = Room.query.all()
+#     result = []
+#     for room in rooms:
+#         users = [ru.user_id for ru in room.users]
+#         result.append({
+#             'id': room.id,
+#             'name': room.name or "",
+#             'image': url_for('static', filename='profile_pics/' + (room.image if room.image else 'default.png'), _external=True),
+#             'doctor_id': room.doctor_id or 0,
+#             'users': users
+#         })
+#     emit('rooms_list', result)
 
-@socketio.on('join_room')
-def handle_join_room(data):
-    join_room(f'room_{data["room_id"]}')
-    emit('status', {'msg': f"User joined room {data['room_id']}"}, room=f'room_{data["room_id"]}')
+# @socketio.on('join_room')
+# def handle_join_room(data):
+#     join_room(f'room_{data["room_id"]}')
+#     emit('status', {'msg': f"User joined room {data['room_id']}"}, room=f'room_{data["room_id"]}')
 
-@socketio.on('leave_room')
-def handle_leave_room(data):
-    leave_room(f'room_{data["room_id"]}')
-    emit('status', {'msg': f"User left room {data['room_id']}"}, room=f'room_{data["room_id"]}')
+# @socketio.on('leave_room')
+# def handle_leave_room(data):
+#     leave_room(f'room_{data["room_id"]}')
+#     emit('status', {'msg': f"User left room {data['room_id']}"}, room=f'room_{data["room_id"]}')
--- hemtna1/app/routes/messages.py
+++ hemtna1/app/routes/messages.py
@@ -1,75 +1,75 @@
-from flask import Blueprint, url_for
-from hemtna1.app import db, socketio
-from hemtna1.app.models import Message, User
-from flask_socketio import emit
+# from flask import Blueprint, url_for
+# from hemtna1.app import db, socketio
+# from hemtna1.app.models import Message, User
+# from flask_socketio import emit
 
-messages_bp = Blueprint('messages', __name__)
+# messages_bp = Blueprint('messages', __name__)
 
-@socketio.on('send_message')
-def handle_send_message(data):
-    msg = Message(
-        message=data['message'],
-        user_id=data['user_id'],
-        doctor_id=data['doctor_id']
-    )
-    db.session.add(msg)
-    db.session.commit()
-    user = User.query.get(msg.user_id)
-    emit('receive_message', {
-        'id': msg.id,
-        'message': msg.message,
-        'timestamp': msg.timestamp.isoformat(),
-        'user_id': msg.user_id,
-        'doctor_id': msg.doctor_id,
-        'username': user.username if user else "",
-        'profile_picture': None if not user or not user.profile_picture else url_for('static', filename='profile_pics/' + user.profile_picture, _external=True)
-    }, room=data.get('room'))
+# @socketio.on('send_message')
+# def handle_send_message(data):
+#     msg = Message(
+#         message=data['message'],
+#         user_id=data['user_id'],
+#         doctor_id=data['doctor_id']
+#     )
+#     db.session.add(msg)
+#     db.session.commit()
+#     user = User.query.get(msg.user_id)
+#     emit('receive_message', {
+#         'id': msg.id,
+#         'message': msg.message,
+#         'timestamp': msg.timestamp.isoformat(),
+#         'user_id': msg.user_id,
+#         'doctor_id': msg.doctor_id,
+#         'username': user.username if user else "",
+#         'profile_picture': None if not user or not user.profile_picture else url_for('static', filename='profile_pics/' + user.profile_picture, _external=True)
+#     }, room=data.get('room'))
 
-@socketio.on('edit_message')
-def handle_edit_message(data):
-    msg = Message.query.get(data['id'])
-    if msg:
-        msg.message = data['new_message']
-        db.session.commit()
-        user = User.query.get(msg.user_id)
-        emit('message_edited', {
-            'id': msg.id,
-            'message': msg.message,
-            'username': user.username if user else "",
-            'profile_picture': None if not user or not user.profile_picture else url_for('static', filename='profile_pics/' + user.profile_picture, _external=True)
-        }, room=data.get('room'))
+# @socketio.on('edit_message')
+# def handle_edit_message(data):
+#     msg = Message.query.get(data['id'])
+#     if msg:
+#         msg.message = data['new_message']
+#         db.session.commit()
+#         user = User.query.get(msg.user_id)
+#         emit('message_edited', {
+#             'id': msg.id,
+#             'message': msg.message,
+#             'username': user.username if user else "",
+#             'profile_picture': None if not user or not user.profile_picture else url_for('static', filename='profile_pics/' + user.profile_picture, _external=True)
+#         }, room=data.get('room'))
 
-@socketio.on('delete_message')
-def handle_delete_message(data):
-    msg = Message.query.get(data['id'])
-    if msg:
-        db.session.delete(msg)
-        db.session.commit()
-        emit('message_deleted', {'id': data['id']}, room=data.get('room'))
+# @socketio.on('delete_message')
+# def handle_delete_message(data):
+#     msg = Message.query.get(data['id'])
+#     if msg:
+#         db.session.delete(msg)
+#         db.session.commit()
+#         emit('message_deleted', {'id': data['id']}, room=data.get('room'))
 
-@socketio.on('get_messages')
-def handle_get_messages(data):
-    room = data.get('room')
-    user_id = data.get('user_id')
-    doctor_id = data.get('doctor_id')
+# @socketio.on('get_messages')
+# def handle_get_messages(data):
+#     room = data.get('room')
+#     user_id = data.get('user_id')
+#     doctor_id = data.get('doctor_id')
 
-    if room:
-        messages = Message.query.all()
-    elif user_id and doctor_id:
-        messages = Message.query.filter_by(user_id=user_id, doctor_id=doctor_id).all()
-    else:
-        messages = Message.query.all()
+#     if room:
+#         messages = Message.query.all()
+#     elif user_id and doctor_id:
+#         messages = Message.query.filter_by(user_id=user_id, doctor_id=doctor_id).all()
+#     else:
+#         messages = Message.query.all()
 
-    result = []
-    for msg in messages:
-        user = User.query.get(msg.user_id)
-        result.append({
-            'id': msg.id,
-            'message': msg.message or "",
-            'timestamp': msg.timestamp.isoformat() if msg.timestamp else "",
-            'user_id': msg.user_id or 0,
-            'doctor_id': msg.doctor_id or 0,
-            'username': user.username if user else "",
-            'profile_picture': None if not user or not user.profile_picture else url_for('static', filename='profile_pics/' + user.profile_picture, _external=True)
-        })
-    emit('messages_list', result)
+#     result = []
+#     for msg in messages:
+#         user = User.query.get(msg.user_id)
+#         result.append({
+#             'id': msg.id,
+#             'message': msg.message or "",
+#             'timestamp': msg.timestamp.isoformat() if msg.timestamp else "",
+#             'user_id': msg.user_id or 0,
+#             'doctor_id': msg.doctor_id or 0,
+#             'username': user.username if user else "",
+#             'profile_picture': None if not user or not user.profile_picture else url_for('static', filename='profile_pics/' + user.profile_picture, _external=True)
+#         })
+#     emit('messages_list', result)
