052bc6db459aa2dffa95ea1dcdcc670b435f1538
Your Name <youremail@example.com>
2025-06-29 05:02:49 +0300

    fix: update config.py for JWT or DB settings

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from hemtna1.app import app, socketio
+from hemtna1.app import create_app, socketio
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+app = create_app()
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
